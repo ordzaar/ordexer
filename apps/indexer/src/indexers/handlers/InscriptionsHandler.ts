d020d3a776f3f4ce21302bb8f4ee1f13ee5d78ff
Daniel Adipranoto <daniel.adipranoto@birthday.dev>
2023-12-13 16:45:10 +0800

    Init inscriptions indexer

--- apps/indexer/src/indexers/handlers/InscriptionsHandler.ts
+++ apps/indexer/src/indexers/handlers/InscriptionsHandler.ts
@@ -1,16 +1,26 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { PrismaPromise } from "@prisma/client";
 
+import { PrismaService } from "../../PrismaService";
 import { VinData, VoutData } from "../types";
 import { BaseIndexerHandler } from "./BaseHandler";
 
 @Injectable()
 export class InscriptionHandler extends BaseIndexerHandler {
-  private readonly logger = new Logger(InscriptionHandler.name);
+  private readonly logger: Logger;
+
+  constructor(
+    private prisma: PrismaService,
+  ) {
+    super();
+    this.logger = new Logger(InscriptionHandler.name);
+  }
 
   async commit(vins: VinData[], vouts: VoutData[], dbOperations: PrismaPromise<any>[]): Promise<void> {
     this.logger.log("commiting insription");
+
+    
   }
 
-  async reorg(fromHeight: number, dbOperations: PrismaPromise<any>[]): Promise<void> { }
+  async reorg(fromHeight: number, dbOperations: PrismaPromise<any>[]): Promise<void> {}
 }
