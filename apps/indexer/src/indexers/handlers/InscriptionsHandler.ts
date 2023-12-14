3a9180a830155beff4c6f3d7daa49eb9546216f7
Daniel Adi <48890526+danadi7@users.noreply.github.com>
2023-12-14 18:23:59 +0800

    feat(indexer): Inscriptions Indexer (#23)
    * Add base indexer class
    
    * Add OutputIndexer and IndexerManager
    
    * init(indexer): init core indexer
    
    * Fix prisma query issue and remove unused imports
    
    * Fix return for OutputIndexer
    
    * Fix cyclic imports
    
    * Merge Index Handlers
    
    * add(indexer): prisma promises to do transaction
    
    * add(indexer): reset prisma promises after commiting indexer
    
    * update(indexer): move bitcoin utils to bitcoin folder
    
    * Remove duplicate rpc provider
    
    * Fix prisma promises in indexers
    
    * Update schema and indexer
    
    * Init inscriptions indexer
    
    * Init inscriptions indexer
    
    * Init inscriptions indexer
    
    * Add utils and packages for inscription indexer
    
    ---------
    
    Co-authored-by: joundy <hafizjoundys@gmail.com>

--- apps/indexer/src/indexers/handlers/InscriptionsHandler.ts
+++ apps/indexer/src/indexers/handlers/InscriptionsHandler.ts
@@ -1,16 +1,225 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { PrismaPromise } from "@prisma/client";
 
+import { OrdInscription, OrdProvider } from "../../ord/providers/OrdProvider";
+import { PrismaService } from "../../PrismaService";
 import { VinData, VoutData } from "../types";
+import { Envelope } from "../utils/Envelope";
+import { getInscriptionFromEnvelope, Inscription as RawInscription } from "../utils/Inscription";
+import { isOIP2Meta, validateOIP2Meta } from "../utils/Oip";
+import { parseLocation } from "../utils/Transaction";
 import { BaseIndexerHandler } from "./BaseHandler";
 
 @Injectable()
 export class InscriptionHandler extends BaseIndexerHandler {
-  private readonly logger = new Logger(InscriptionHandler.name);
+  private readonly logger: Logger;
+
+  constructor(
+    private prisma: PrismaService,
+    private ord: OrdProvider,
+  ) {
+    super();
+    this.logger = new Logger(InscriptionHandler.name);
+  }
 
   async commit(vins: VinData[], vouts: VoutData[], dbOperations: PrismaPromise<any>[]): Promise<void> {
     this.logger.log("commiting insription");
+
+    const { height } = vins[vins.length - 1].block;
+
+    await this.ord.waitForBlock(height);
+
+    const inscriptions = await this.getInscriptions(vins);
+
+    await this.insertInscriptions(inscriptions, dbOperations);
+
+    await this.transferInscriptions(
+      vins.map(({ vout }) => `${vout.txid}:${vout.n}`),
+      dbOperations,
+    );
   }
 
-  async reorg(fromHeight: number, dbOperations: PrismaPromise<any>[]): Promise<void> { }
+  async reorg(fromHeight: number, dbOperations: PrismaPromise<any>[]): Promise<void> {
+    dbOperations.push(
+      this.prisma.inscription.deleteMany({
+        where: {
+          height: {
+            gte: fromHeight,
+          },
+        },
+      }),
+    );
+  }
+
+  async getInscriptions(vins: VinData[]) {
+    const envelopes: Envelope[] = [];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const vin of vins) {
+      const envelope = Envelope.fromTxinWitness(vin.txid, vin.witness);
+      if (envelope && envelope.isValid) {
+        envelopes.push(envelope);
+      }
+    }
+
+    const ordData = new Map<string, OrdInscription>();
+    const chunkSize = 5_000;
+    for (let i = 0; i < envelopes.length; i += chunkSize) {
+      const chunk = envelopes.slice(i, i + chunkSize);
+      const data = await this.ord.getInscriptionsForIds(chunk.map((item) => item.id));
+      // eslint-disable-next-line no-restricted-syntax
+      for (const item of data) {
+        ordData.set(item.inscription_id, item);
+      }
+    }
+
+    const inscriptions: RawInscription[] = [];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const envelope of envelopes) {
+      const inscription = await getInscriptionFromEnvelope(envelope, ordData);
+      if (inscription !== undefined) {
+        inscriptions.push(inscription);
+      }
+    }
+    return inscriptions;
+  }
+
+  async insertInscriptions(rawInscriptions: RawInscription[], dbOperations: PrismaPromise<any>[]) {
+    const inscriptions: Inscription[] = [];
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const inscription of rawInscriptions) {
+      const output = await this.prisma.output.findUniqueOrThrow({
+        where: {
+          voutTxid_voutTxIndex: {
+            voutTxid: inscription.outpoint.split(":")[0],
+            voutTxIndex: parseInt(inscription.outpoint.split(":")[1], 10),
+          },
+        },
+      });
+      const entry: Partial<Inscription> = {
+        inscriptionId: inscription.id,
+        creator: inscription.creator,
+        owner: inscription.owner,
+        sat: inscription.sat,
+        mimeType: inscription.media.mime.type,
+        mimeSubtype: inscription.media.mime.subtype,
+        mediaType: inscription.media.type,
+        mediaCharset: inscription.media.charset,
+        mediaSize: inscription.media.size,
+        mediaContent: inscription.media.content,
+        timestamp: inscription.timestamp,
+        height: inscription.height,
+        fee: inscription.fee,
+        genesis: inscription.genesis,
+        number: inscription.number,
+        outpoint: inscription.outpoint,
+        ometa: inscription.meta,
+        outputId: output.id,
+      };
+      if (inscription.oip) {
+        entry.meta = inscription.oip;
+        if (isOIP2Meta(inscription.oip)) {
+          entry.verified = await validateOIP2Meta(inscription.oip);
+        }
+      }
+
+      inscriptions.push(entry as Inscription);
+    }
+
+    dbOperations.push(
+      this.prisma.inscription.createMany({
+        data: inscriptions,
+        skipDuplicates: true,
+      }),
+    );
+  }
+
+  async transferInscriptions(outpoints: string[], dbOperations: PrismaPromise<any>[]) {
+    if (outpoints.length === 0) {
+      return 0;
+    }
+
+    let count = 0;
+
+    const chunkSize = 10_000;
+    for (let i = 0; i < outpoints.length; i += chunkSize) {
+      const chunk = outpoints.slice(i, i + chunkSize);
+      const docs = await this.prisma.inscription.findMany({
+        where: {
+          outpoint: {
+            in: chunk,
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+      await this.commitTransfers(
+        docs.map((doc) => doc.id),
+        dbOperations,
+      );
+      count += docs.length;
+    }
+
+    return count;
+  }
+
+  async commitTransfers(ids: string[], dbOperations: PrismaPromise<any>[]) {
+    const chunkSize = 5_000;
+    for (let i = 0; i < ids.length; i += chunkSize) {
+      const chunk = ids.slice(i, i + chunkSize);
+      const data = await this.ord.getInscriptionsForIds(chunk);
+      // eslint-disable-next-line no-restricted-syntax
+      for (const item of data) {
+        const [txid, n] = parseLocation(item.satpoint);
+        const output = await this.prisma.output.findUnique({
+          where: {
+            voutTxid_voutTxIndex: {
+              voutTxid: txid,
+              voutTxIndex: n,
+            },
+          },
+        });
+
+        dbOperations.push(
+          this.prisma.inscription.update({
+            where: {
+              inscriptionId: item.inscription_id,
+            },
+            data: {
+              owner: output?.addresses[0] ?? "",
+              outpoint: `${txid}:${n}`,
+            },
+          }),
+        );
+      }
+    }
+  }
 }
+
+export type Inscription = {
+  inscriptionId: string;
+  parent?: string;
+  children: string[];
+  creator: string;
+  owner?: string;
+  sat: number;
+  mimeType: string;
+  mimeSubtype: string;
+  mediaType: string;
+  mediaCharset: string;
+  mediaSize: number;
+  mediaContent: string;
+  timestamp: number;
+  height: number;
+  fee: number;
+  genesis: string;
+  number: number;
+  sequence: number;
+  outpoint: string;
+  ethereum: string;
+  ometa?: any;
+  meta?: any;
+  verified?: boolean;
+  outputId: string;
+};
---
+++ apps/indexer/src/indexers/utils/Inscription.ts
@@ -0,0 +1,174 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable consistent-return */
+import { ConfigService } from "@nestjs/config";
+
+import { RawTransaction } from "../../bitcoin/BitcoinService";
+import { OrdInscription } from "../../ord/providers/OrdProvider";
+import { PrismaService } from "../../PrismaService";
+import { VinData } from "../types";
+import { Envelope } from "./Envelope";
+import { parseLocation } from "./Transaction";
+
+const config = new ConfigService();
+const prisma = new PrismaService(config);
+
+export class Inscription {
+  readonly id: string;
+
+  readonly parent?: string;
+
+  readonly children?: string[];
+
+  readonly genesis: string;
+
+  readonly creator: string;
+
+  readonly owner: string;
+
+  readonly media: InscriptionMedia;
+
+  readonly number: number;
+
+  readonly sequence: number;
+
+  readonly height: number;
+
+  readonly fee: number;
+
+  readonly sat: number;
+
+  readonly outpoint: string;
+
+  readonly timestamp: number;
+
+  readonly meta?: Object;
+
+  readonly oip?: Object;
+
+  constructor(data: InscriptionData) {
+    this.id = data.id;
+    this.parent = data.parent;
+    this.children = data.children;
+    this.genesis = data.genesis;
+    this.creator = data.creator;
+    this.owner = data.owner;
+    this.media = data.media;
+    this.number = data.number;
+    this.sequence = data.sequence;
+    this.height = data.height;
+    this.fee = data.fee;
+    this.sat = data.sat;
+    this.outpoint = data.outpoint;
+    this.timestamp = data.timestamp;
+    this.meta = data.meta;
+    this.oip = data.oip;
+  }
+
+  static async fromTransaction(tx: RawTransaction) {
+    const envelope = Envelope.fromTransaction(tx);
+    if (envelope && envelope.isValid) {
+      return envelope;
+    }
+  }
+
+  static async fromVin(vin: VinData) {
+    const envelope = Envelope.fromTxinWitness(vin.txid, vin.witness);
+    if (envelope && envelope.isValid) {
+      return envelope;
+    }
+  }
+}
+
+type InscriptionData = {
+  id: string;
+  parent?: string;
+  children?: string[];
+  genesis: string;
+  creator: string;
+  owner: string;
+  media: InscriptionMedia;
+  number: number;
+  sequence: number;
+  height: number;
+  fee: number;
+  sat: number;
+  outpoint: string;
+  timestamp: number;
+  meta: Object;
+  oip?: Object;
+};
+
+type InscriptionMedia = {
+  type: string;
+  charset: string;
+  mime: {
+    type: string;
+    subtype: string;
+  };
+  content: string;
+  size: number;
+};
+
+export async function getInscriptionFromEnvelope(
+  envelope: Envelope,
+  ord: Map<string, OrdInscription>,
+): Promise<Inscription | undefined> {
+  const ordData = ord.get(envelope.id);
+  if (ordData === undefined) {
+    return undefined;
+  }
+
+  const [locationTxid, locationN] = parseLocation(ordData.satpoint);
+
+  return new Inscription({
+    id: envelope.id,
+    parent: envelope.parent,
+    children: [],
+    genesis: envelope.txid,
+    creator: await getInscriptionCreator(envelope.txid),
+    owner: await getInscriptionOwner(locationTxid, locationN),
+    media: {
+      type: envelope.media.type ?? "",
+      charset: envelope.media.charset,
+      mime: {
+        type: envelope.media.mimeType,
+        subtype: envelope.media.mimeSubtype,
+      },
+      content: envelope.content?.body ?? "",
+      size: envelope.content?.size ?? 0,
+    },
+    number: ordData.number,
+    sequence: ordData.sequence,
+    height: ordData.genesis_height,
+    fee: ordData.genesis_fee,
+    sat: ordData.sat,
+    outpoint: `${locationTxid}:${locationN}`,
+    timestamp: ordData.timestamp,
+    meta: envelope.meta,
+    oip: envelope.oip,
+  });
+}
+
+async function getInscriptionCreator(txid: string) {
+  const data = await prisma.output.findUnique({
+    where: {
+      voutTxid_voutTxIndex: {
+        voutTxid: txid,
+        voutTxIndex: 0,
+      },
+    },
+  });
+  return data?.addresses[0] ?? "";
+}
+
+async function getInscriptionOwner(txid: string, n: number) {
+  const data = await prisma.output.findUnique({
+    where: {
+      voutTxid_voutTxIndex: {
+        voutTxid: txid,
+        voutTxIndex: n,
+      },
+    },
+  });
+  return data?.addresses[0] ?? "";
+}
