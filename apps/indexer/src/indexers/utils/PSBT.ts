74314089a56574df797d31695946c6f1b3b99492
Daniel <daniel@adip.me>
2024-01-09 01:46:09 +0900

    Move btc-sat converters to Bitcoin Service utils

--- apps/indexer/src/indexers/utils/PSBT.ts
+++ apps/indexer/src/indexers/utils/PSBT.ts
@@ -1,9 +1,7 @@
 import { HttpService } from "@nestjs/axios";
 import { ConfigService } from "@nestjs/config";
-import { BitcoinService } from "@ordzaar/bitcoin-service";
+import { BitcoinService, btcToSat } from "@ordzaar/bitcoin-service";
 import { address, networks, Psbt, Transaction } from "bitcoinjs-lib";
-
-import { btcToSat } from "./Bitcoin";
 
 const bitcoinService = new BitcoinService(new ConfigService(), new HttpService());
 
--- packages/bitcoin-service/src/index.ts
+++ packages/bitcoin-service/src/index.ts
@@ -2,4 +2,5 @@ export * from "./BitcoinModule";
 export * from "./BitcoinService";
 export * from "./errors/RpcError";
 export * from "./utils/Address";
+export * from "./utils/Converter";
 export * from "./utils/Transaction";
